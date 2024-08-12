4e50bf9d119f4e6b14a71368b5b37b943a1479fb
OliveADay <sharinggon23@gmail.com>
2024-08-11 22:50:42 -0700

    s

--- Movment.gd
+++ Movment.gd
@@ -3,13 +3,38 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
+var batDown = false
+@export var currentBall = RigidBody2D
+var balls = [RigidBody2D]
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	currentBall = $ball
+	get_children()
+
+func _process(delta):
+	$Bat.look_at(get_viewport().get_mouse_position())
+	balls = find_children("ball", "RigidBody2D")
+	if $Area2D.has_overlapping_bodies():
+		for body in $Area2D.get_overlapping_bodies():
+			if body.is_in_group("ball"):
+				for ball in balls:
+					if body == ball:
+						currentBall = ball
+						currentBall.linear_damp = 0
+	
 
 func _physics_process(delta):
 	# Add the gravity.
+	if Input.is_action_just_pressed("shoot"):
+		HandleBat()
+	if !batDown:
+		$AnimationPlayer_Bat.play("idle_1")
+	if batDown:
+		$AnimationPlayer_Bat.play("Idle_2")
 	
 	var Xaxis = Input.get_axis("move_left", "move_right")
 	var Yaxis = Input.get_axis("move_down", "move_up")
@@ -23,9 +48,29 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
-	if Xaxis > 0:
+	if position.x - get_viewport().get_mouse_position().x < 0:
 		$Sprite2D.flip_h = false
-	if Xaxis < 0:
+	else:
 		$Sprite2D.flip_h = true
 
 	move_and_slide()
+	if currentBall != null:
+		currentBall.linear_velocity = velocity
+		
+	
+	
+func HandleBat():
+	if !batDown:
+		$AnimationPlayer_Bat.play("Swing_down")
+	else:
+		$AnimationPlayer_Bat.play("Swing_up")	
+	if currentBall != null:
+		currentBall.apply_central_impulse((get_viewport().get_mouse_position() - position).normalized() *100)
+		currentBall.linear_damp = 0.5
+		currentBall.remove_from_group("ball")
+		
+		#currentBall.linear_velocity = (get_viewport().get_mouse_position() - position).normalized() * 10
+	currentBall = null
+	batDown = !batDown
+	
+	
---
+++ ball.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if linear_velocity == Vector2(0,0):
+		add_to_group("ball")
