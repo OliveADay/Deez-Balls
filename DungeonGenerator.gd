ffa6b72e5ce2d1ecebfc1470ac4981b8c7ffae8a
OliveADay <sharinggon23@gmail.com>
2024-09-03 13:54:54 -0700

    s

--- DungeonGenerator.gd
+++ DungeonGenerator.gd
@@ -1,9 +1,12 @@
 extends Node2D
-
+var rectTest = Rect2i(0,0,10,10)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TileMap/Layer1.set_cell(Vector2i(0,5), 0, Vector2i(0,0)) # Replace with function body.
+	for y in 255:
+		for x in 255:
+			if rectTest.has_point(Vector2i(x,y)):
+				$TileMap/Layer1.set_cell(Vector2i(x,y), 0, Vector2i(0,0)) # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
