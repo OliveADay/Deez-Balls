314b6d0be72683f6e5b564c0f34f585ed3c935c1
OliveADay <sharinggon23@gmail.com>
2024-09-04 11:03:27 -0700

    s

--- DungeonGenerator.gd
+++ DungeonGenerator.gd
@@ -5,32 +5,41 @@ var rng = RandomNumberGenerator.new()
 var GridAttempts
 var rectTests: Array[Rect2i] = []
 var rectTests_inter: Array[Rect2i] = []
-var rectAttempts = 5
+var rectAttempts = 1000
 var rectMinsandMaxes = [-64,64, 5, 10]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in rectAttempts:
-		var rectpos = rng.randi_range(rectMinsandMaxes[0],rectMinsandMaxes[1])
-		var rectsize = rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3])
-		var recti = Rect2i(rectpos, rectpos, rectsize,rectsize)
+		var rectpos = Vector2i(rng.randi_range(rectMinsandMaxes[0],rectMinsandMaxes[1]), rng.randi_range(rectMinsandMaxes[0],rectMinsandMaxes[1]))
+		var rectsize = [rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3]), rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3])]
+		var recti = Rect2i(rectpos.x, rectpos.y, rectsize[0],rectsize[1])
 		var colliding_other_rects = false
-		for b in range(rectTests_inter.size()):
-			if rectTests_inter[b].intersects(recti):
+		for b in range(rectTests.size()):
+			if rectTests[b].intersects(recti):
 				colliding_other_rects = true
 		if !colliding_other_rects:
-			rectTests_inter.append(recti)
+			rectTests.append(recti)
+			var rectinner = Rect2i(recti.position.x+1, recti.position.y+1, recti.size.x-2, recti.size.y-2)
+			rectTests_inter.append(rectinner)
 	
 	for y in 257:
 		for x in 257:
-			if rectTest.has_point(Vector2i(x-128,y-128)) and !rectTest_inter.has_point(Vector2i(x-128,y-128)):
-				var opening = rng.randi_range(1, 8*2) #replace 8 with rect width once that is figured out, currently just testing
-				var cellId = 0
-				if opening == 2:
-					cellId = 2
-				else:
-					cellId = 0
-				$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), cellId, Vector2i(0,0)) # Replace with function body.
+			var inRect = false
+			for i in range(rectTests.size()):				
+				if rectTests[i].has_point(Vector2i(x-128,y-128)):
+					var opening = rng.randi_range(1, rectTests[i].size.x * 2) #replace 8 with rect width once that is figured out, currently just testing
+					var cellId = 0
+					if opening == 2:
+						cellId = -1
+					else:
+						cellId = -1
+						
+					$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), cellId, Vector2i(0,0)) # Replace with function body.
+					inRect = true
+			if !inRect:
+				$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), 0, Vector2i(0,0))
+					
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
