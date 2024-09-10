8a1b88d06aca2c35ad37059b0f5144c024188c42
OliveADay <sharinggon23@gmail.com>
2024-09-10 13:28:27 -0700

    s

--- DungeonGenerator.gd
+++ DungeonGenerator.gd
@@ -12,8 +12,11 @@ var rectMinsandMaxes = [-64,64, 7, 20]
 func _ready() -> void:
 	var rectpos_1 = Vector2i(-2,-2)
 	var rectsize_1 = [rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3]), rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3])]
+	
 	var rect_1 = Rect2i(rectpos_1.x, rectpos_1.y, rectsize_1[0], rectsize_1[1])
+	var rect_1_outer = Rect2i(rectpos_1.x -1, rectpos_1.y -1, rectsize_1[0] + 2, rectsize_1[1] + 2)
 	rectTests.append(rect_1)
+	rectTests_inter.append(rect_1)
 	
 	
 	for i in rectAttempts:
@@ -21,13 +24,22 @@ func _ready() -> void:
 		var rectsize = [rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3]), rng.randi_range(rectMinsandMaxes[2],rectMinsandMaxes[3])]
 		var recti = Rect2i(rectpos.x, rectpos.y, rectsize[0],rectsize[1])
 		var colliding_other_rects = false
+		var nextTo_Otherrects = false
 		for b in range(rectTests.size()):
+			var outerRect = Rect2i(rectTests[b].position.x -1, rectTests[b].position.y -1, rectTests[b].size.x  +2, rectTests[b].size.y  +2)
 			if rectTests[b].intersects(recti):
 				colliding_other_rects = true
+			if outerRect.intersects(recti):
+				nextTo_Otherrects = true
+			
 		if !colliding_other_rects:
 			rectTests.append(recti)
 			#var rectinner = Rect2i(recti.position.x+1, recti.position.y+1, recti.size.x-2, recti.size.y-2)
 			#rectTests_inter.append(rectinner)
+	
+	for x in rectTests.size() -1:
+		if rectTests[x+1].intersects(rect_1_outer):
+			rectTests_inter.append(rectTests[x+1])
 	
 	for y in 257:
 		for x in 257:
@@ -54,6 +66,7 @@ func _ready() -> void:
 					#$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), -1, Vector2i(0,0))
 				#if $TileMap/Layer1.get_cell_source_id(Vector2i(x-128,y-127)) != 0 and $TileMap/Layer1.get_cell_source_id(Vector2i(x-128,y-129)) != 0:
 					#$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), -1, Vector2i(0,0))
+	print(rectTests_inter)
 					
 
 
