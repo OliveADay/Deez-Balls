86b884b487b0b53c622c12e0c4940d50b6c582da
OliveADay <sharinggon23@gmail.com>
2024-09-11 11:04:41 -0700

    s

--- DungeonGenerator.gd
+++ DungeonGenerator.gd
@@ -5,7 +5,7 @@ var rng = RandomNumberGenerator.new()
 var GridAttempts
 var rectTests: Array[Rect2i] = []
 var rectTests_inter: Array[Rect2i] = []
-var rectAttempts = 10000
+@export var rectAttempts = 5000
 var rectMinsandMaxes = [-64,64, 7, 20]
 
 # Called when the node enters the scene tree for the first time.
@@ -29,7 +29,7 @@ func _ready() -> void:
 			var outerRect = Rect2i(rectTests[b].position.x -1, rectTests[b].position.y -1, rectTests[b].size.x  +2, rectTests[b].size.y  +2)
 			if rectTests[b].intersects(recti):
 				colliding_other_rects = true
-			if outerRect.intersects(recti):
+			if outerRect.intersection(recti).size.x > 1:
 				nextTo_Otherrects = true
 			
 		if !colliding_other_rects and nextTo_Otherrects:
@@ -37,36 +37,34 @@ func _ready() -> void:
 			#var rectinner = Rect2i(recti.position.x+1, recti.position.y+1, recti.size.x-2, recti.size.y-2)
 			#rectTests_inter.append(rectinner)
 	
-	for x in rectTests.size() -1:
-		if rectTests[x+1].intersects(rect_1_outer):
-			rectTests_inter.append(rectTests[x+1])
+	#for rect in rectTests.size()-1:
+		#var rectIntersects = 0;
+		#var outerRect = Rect2i(rectTests[rect+1].position.x-1, rectTests[rect+1].position.y-1, rectTests[rect+1].size.x+2, rectTests[rect+1].size.y+2)
+		#for rectChecked in rectTests.size()-1:
+		#	if outerRect.intersects(rectTests[rectChecked+1]):
+		#		rectIntersects +=1
+		#if rectIntersects > 2:
+		#	rectTests_inter.append(rectTests[rect +1])
+	#print(rectTests_inter.size())
 	
 	for y in 257:
 		for x in 257:
 			var inRect = false
 			for i in range(rectTests.size()):				
-				if rectTests[i].has_point(Vector2i(x-128,y-128)):
-					var opening = rng.randi_range(1, rectTests[i].size.x * 2) #replace 8 with rect width once that is figured out, currently just testing
-					var cellId = 0
-					if opening == 2:
-						cellId = -1
-					else:
-						cellId = -1
-						
-					$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), cellId, Vector2i(0,0)) # Replace with function body.
+				if rectTests[i].has_point(Vector2i(x-128,y-128)):	
+					$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), -1, Vector2i(0,0)) # Replace with function body.
 					inRect = true
 			if !inRect:
 				pass
 				$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), 0, Vector2i(0,0))
 				
-	#for y in 257:
+	#for y in 257: so this seems to have made tiles that were surrounded by 2 empty tiles either directly above and below or right and left of a tile, that tile would then erase itself
 		#for x in 257:
 			#if $TileMap/Layer1.get_cell_source_id(Vector2i(x-128,y-128)) == 0:
 				#if $TileMap/Layer1.get_cell_source_id(Vector2i(x-127,y-128)) != 0 and $TileMap/Layer1.get_cell_source_id(Vector2i(x-129,y-128)) != 0:
 					#$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), -1, Vector2i(0,0))
 				#if $TileMap/Layer1.get_cell_source_id(Vector2i(x-128,y-127)) != 0 and $TileMap/Layer1.get_cell_source_id(Vector2i(x-128,y-129)) != 0:
 					#$TileMap/Layer1.set_cell(Vector2i(x-128,y-128), -1, Vector2i(0,0))
-	print(rectTests_inter)
 					
 
 
