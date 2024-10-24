c2ee6b50956da1275616ae61e009450a4b264bf8
OliveADay <sharinggon23@gmail.com>
2024-10-24 07:33:27 -0700

    s

--- spidy.gd
+++ spidy.gd
@@ -4,11 +4,13 @@ var rng = RandomNumberGenerator.new()
 var player_seen = false
 var player_seen_prev = false
 var attack_mode = false
+var attack_prev_mode = false
 var animationTime=2
 var startPos = Vector2(0,0)
 var startRect = Rect2i()
 var timeframe = 0
 var spreadSpeed = 4
+var inRect = true
 @onready var player= get_tree().get_first_node_in_group("player")
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +22,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var playerPos = Vector2i(int(player.position.x/16),int(player.position.y/16))
-	if startRect.has_point(playerPos):
+	var posi = Vector2i(int(position.x/16),int(position.y/16))
+	if startRect.has_point(playerPos) and startRect.has_point(posi):
 		player_seen = true
 	else:
 		player_seen=false
@@ -49,10 +52,12 @@ func _process(delta: float) -> void:
 		$PointLight2D2.texture_scale=0.3
 	var direction = Vector2(player.position.x - position.x,player.position.y-position.y)
 	if attack_mode:
-		linear_velocity = direction.normalized()*50
+		if not attack_prev_mode and inRect:
+			add_constant_central_force(direction.normalized()*3)
 		look_at(player.position)
 		$AnimationPlayer.play("walk")
-		
+	
+	
 	if not player_seen:
 		var i = rng.randi_range(0,2)
 		var idles = ['idle','idle_2','idle_3']
@@ -61,5 +66,5 @@ func _process(delta: float) -> void:
 		else:
 			$AnimationPlayer.play(idles[i])
 			animationTime=2
-			
+	attack_prev_mode = attack_mode
 	player_seen_prev = player_seen
