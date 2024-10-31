6a6daa5f9207b140593f7d41d3ce739851ea70c2
OliveADay <sharinggon23@gmail.com>
2024-10-31 15:33:02 -0700

    s

--- DungeonGenerator.gd
+++ DungeonGenerator.gd
@@ -47,6 +47,7 @@ func _ready() -> void:
 				var yPos = rng.randi_range(recti.position.y, recti.size.y+recti.position.y)
 				spidys_pos.append(Vector2(xPos*16,yPos*16))
 				spidys.append(spidy)
+				jamiePlaced = true
 		
 	var randomer = RandomNumberGenerator.new()
 	var index = randomer.randi_range(0,rectTests.size() -1)
@@ -78,10 +79,10 @@ func _ready() -> void:
 							$Layer1.set_cell(Vector2i(x-128,y-128), 0, Vector2i(0,1))
 						else:
 							$Layer1.set_cell(Vector2i(x-128,y-128), 0, Vector2i(1,0))		
-						if (abs((x-128)-rectFin.get_center().x) < 4 and abs((y-128)-rectFin.get_center().y) < 4) and not jamiePlaced and not (abs((x-128)-rectFin.get_center().x) < 3 and abs((y-128)-rectFin.get_center().y) < 3):
+						if (abs((x-128)-rectFin.get_center().x) < 5 and abs((y-128)-rectFin.get_center().y) < 5) and not jamiePlaced and not (abs((x-128)-rectFin.get_center().x) < 4 and abs((y-128)-rectFin.get_center().y) < 4):
 							var jamie = ResourceLoader.load("res://Jamie.tscn").instantiate()
 							add_child(jamie)
-							jamie.position = Vector2(rectFin.get_center().x*16,rectFin.get_center().y*16)
+							jamie.position = Vector2((x-128)*16,(y-128)*16)
 							jamiePlaced=true				
 					else:
 						$Layer1.set_cell(Vector2i(x-128,y-128), 0, Vector2i(1,0))
